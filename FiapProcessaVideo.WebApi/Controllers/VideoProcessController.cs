bd93ff4d65b7dfda529e5b96f3f34d68e77f1e42
Rafael Yuji Nakazono <rafa.yuji@gmail.com>
2025-01-25 23:18:27 -0300

    implementing notification publisher - wip

--- FiapProcessaVideo.WebApi/Controllers/VideoProcessController.cs
+++ FiapProcessaVideo.WebApi/Controllers/VideoProcessController.cs
@@ -1,5 +1,7 @@
 using FiapProcessaVideo.Application.UseCases;
 using FiapProcessaVideo.Domain;
+using FiapProcessaVideo.Infrastructure.Messaging.Publishers;
+using FiapProcessaVideo.Infrastructure.Messaging.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FiapProcessaVideo.WebApi.Controllers
@@ -9,10 +11,12 @@ namespace FiapProcessaVideo.WebApi.Controllers
     public class VideoProcessController : ControllerBase
     {
         private readonly IProcessVideoUseCase _processVideoUseCase;
+        private readonly NotificationPublisher _notificationPublisher;
 
-        public VideoProcessController(IProcessVideoUseCase processVideoUseCase)
+        public VideoProcessController(IProcessVideoUseCase processVideoUseCase, NotificationPublisher notificationPublisher)
         {
             _processVideoUseCase = processVideoUseCase;
+            _notificationPublisher = notificationPublisher;
         }
 
         [HttpPost("process")]
@@ -20,6 +24,14 @@ namespace FiapProcessaVideo.WebApi.Controllers
         {
             Video video = Video.Load(@"", videoKey, new TimeSpan(240), new TimeSpan(10));
             var result = await _processVideoUseCase.Execute(video);
+
+            NotificationCreatedEvent notificationCreatedEvent = new NotificationCreatedEvent();
+            notificationCreatedEvent.Id = Guid.NewGuid().ToString();
+            notificationCreatedEvent.Email = "rafa.yuji@gmail.com";
+            notificationCreatedEvent.File = videoKey;
+            notificationCreatedEvent.Status = "processed";
+            notificationCreatedEvent.UserId = "";
+            _notificationPublisher.PublishNotificationCreated(notificationCreatedEvent);
             return Ok(new { ZipFilePath = result });
         }
     }
